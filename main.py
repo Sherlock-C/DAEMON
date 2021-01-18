9011bca94513bbeb6a6cad949725a10b56c44ca9
Sherlock-C <77371663+Sherlock-C@users.noreply.github.com>
2021-01-18 17:49:07 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 from etalon.data_utils import *
 from etalon.options import Options
-from etalon.model import *
+from etalon.model_lstm_gru import *
 from torch.utils.data import DataLoader
 import os
 
@@ -105,7 +105,7 @@ def anomaly_detection(data_type):
 
             test_data = DataLoader(dataset=samples_test_data, batch_size=opt.test_batchsize)
 
-            model = Detection(opt, train_data, val_data, test_data, test_label, device)
+            model = ETALON(opt, train_data, val_data, test_data, test_label, device)
 
             train_time, epoch_time = model.train()
 
